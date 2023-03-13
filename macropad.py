fd84127a3f6449d28735d47af9b2abc0fb3bc4dc
Siegband <114338337+Sigeband@users.noreply.github.com>
2023-03-13 16:29:14 +0100

    added 3 button instead of 1

--- macropad.py
+++ macropad.py
@@ -5,23 +5,43 @@ import usb_hid
 from keycode_win_de import Keycode
 from adafruit_hid.keyboard import Keyboard
 from keyboard_layout_win_de import KeyboardLayout
-
 keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayout(keyboard)
+keyboard = Keyboard(usb_hid.devices)
 
 btn1_pin = board.GP15
+btn2_pin = board.GP14
+btn3_pin = board.GP13
 
-keyboard = Keyboard(usb_hid.devices)
 
 btn1 = digitalio.DigitalInOut(btn1_pin)
 btn1.direction = digitalio.Direction.INPUT
 btn1.pull = digitalio.Pull.DOWN
 
+btn2 = digitalio.DigitalInOut(btn2_pin)
+btn2.direction = digitalio.Direction.INPUT
+btn2.pull = digitalio.Pull.DOWN
+
+btn3 = digitalio.DigitalInOut(btn3_pin)
+btn3.direction = digitalio.Direction.INPUT
+btn3.pull = digitalio.Pull.DOWN
+
 while True:
     if btn1.value:
-        print("button pressed")
-        keyboard.press(Keycode.D)
+        print("button1 pressed")
+        keyboard.press(Keycode.A)
         time.sleep(0.5)
-        keyboard.release(Keycode.D)
+        keyboard.release(Keycode.A)
         
-time.sleep(0.1)   
+    if btn2.value:
+        print("button2 pressed")
+        keyboard.press(Keycode.B)
+        time.sleep(0.5)
+        keyboard.release(Keycode.B)
+    if btn3.value:
+        print("button3 pressed")
+        keyboard.press(Keycode.C)
+        time.sleep(0.5)
+        keyboard.release(Keycode.C)
+        
+time.sleep(0.1)  
