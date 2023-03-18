a761c70ea0ad7f5869db79f2c9fcb78b3bea5ff9
Siegband <114338337+Sigeband@users.noreply.github.com>
2023-03-18 22:59:48 +0100

    working with switch

--- macropad.py
+++ macropad.py
@@ -12,11 +12,13 @@ keyboard = Keyboard(usb_hid.devices)
 btn1_pin = board.GP15
 btn2_pin = board.GP14
 btn3_pin = board.GP13
+switch_pin = board.GP12
 
 
 btn1 = digitalio.DigitalInOut(btn1_pin)
 btn1.direction = digitalio.Direction.INPUT
 btn1.pull = digitalio.Pull.DOWN
+
 
 btn2 = digitalio.DigitalInOut(btn2_pin)
 btn2.direction = digitalio.Direction.INPUT
@@ -26,7 +28,18 @@ btn3 = digitalio.DigitalInOut(btn3_pin)
 btn3.direction = digitalio.Direction.INPUT
 btn3.pull = digitalio.Pull.DOWN
 
+switch1 = digitalio.DigitalInOut(switch_pin)
+switch1.direction = digitalio.Direction.INPUT
+switch1.pull = digitalio.Pull.DOWN
+
+prev_state1 = switch1.value
+
+
+
+
 while True:
+    cur_state1 = switch1.value
+    
     if btn1.value:
         print("button1 pressed")
         keyboard.press(Keycode.A)
@@ -44,4 +57,11 @@ while True:
         time.sleep(0.5)
         keyboard.release(Keycode.C)
         
-time.sleep(0.1)  
+    if cur_state1 != prev_state1:
+        print("pressed")
+        keyboard.press(Keycode.J)
+        time.sleep(0.5)
+        keyboard.release(Keycode.J)
+        prev_state1 = switch1.value
+        
+time.sleep(0.1)
